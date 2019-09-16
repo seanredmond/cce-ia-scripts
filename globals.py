599055fee7ed073973b16649a91565f868cbaecb
Sean Redmond <github-smr@sneakemail.com>
2019-09-16 10:59:17 -0400

    Define TSV fields in a separate file

---
+++ globals.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+TSV_FIELDS=['ia_id', 'cce_id', 'match_quality', 'ia-book', 'ia-date',
+            'ia-openlibrary', 'ia-lccn', 'ia-oclc', 'cce_id',
+            'cce-regnum', 'cce-regdate', 'cce-newmatter', 'cce-entry',
+            'oclc-book', 'oclc-owi', 'oclc-main-id', 'oclc-type',
+            'oclc-editions', 'oclc-holdings', 'oclc-eholdings',
+            'oclc-first-date', 'oclc-last-date', 'has-older-date',
+            'oclc-lccns', 'hathi-full-view']
